98ad9992b27c3d4085edb008d77276111393d30e
Qiuzao Zhang <qiuzaozhang@gmail.com>
2014-07-22 16:59:42 -0400

    add designer coverletter

--- js/main.js
+++ js/main.js
@@ -39,6 +39,8 @@ $(function() {
 });
 
 
+
+
 (function($) {
 
   /**
@@ -82,4 +84,20 @@ $(function() {
 
 
 
-
+jQuery(document).ready(function() {
+    var offset = 760;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
